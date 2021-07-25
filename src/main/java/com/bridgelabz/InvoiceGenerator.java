7b7bb9f71481ec2bda1fc7f6994cfcfe569a915a
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-25 20:10:19 +0530

    Added: To calculate total fare

---
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class InvoiceGenerator {
+
+	private static final int costPerMinute =1;
+	private static final double minCostPerKm =10;
+	private static final double minfare = 5;
+	
+	public double calculateFare(double distance, int minute) {
+		double totalfare = (distance * minCostPerKm) + (minute * costPerMinute);
+		
+		if(distance < 1.0 && minute <= 5) {
+			return totalfare;
+		}else if(distance < 0.5 && minute <= 2) {
+			return minfare;
+		}
+		return totalfare;
+	}
+	
+}
--- src/test/java/com/bridgelabz/InvoiceServicesTest.java
+++ src/test/java/com/bridgelabz/InvoiceServicesTest.java
@@ -1,5 +1,26 @@
 package com.bridgelabz;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class InvoiceServicesTest {
 
+	@Test
+	public void givenDistanceAndMinute_shouldReturn_totalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 20.0;
+		int minute = 15;
+		double fare = invoiceGenerator.calculateFare(distance, minute);
+		assertEquals(215, fare,  0.0);	
+	}
+	
+	@Test
+	public void givenDistanceAndMinute_shouldReturn_minFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.3;
+		int minute = 2;
+		double fare = invoiceGenerator.calculateFare(distance, minute);
+		assertEquals(5, fare,  0.0);	
+	}
 }
